e773d6e1b25aedbe0bb0ffda34be1c84ca16c0d8
Marina <marinaakter878@gmail.com>
2024-10-04 16:40:42 +0600

    video.js to dynamically display video details and add a button for more information

--- scripts/video.js
+++ scripts/video.js
@@ -49,12 +49,27 @@ const loadcategoryVideos = (id) => {
     .catch((error) => console.log(error));
 }
 
+const loadDetails = async (videoId) => {
+    console.log(videoId);
+    const url = `https://openapi.programming-hero.com/api/phero-tube/video${videoId}`
+    const res = await fetch(url)
+    const data = await res.json()
+    displayDetails(data.video)
+    
+
+}
+
+const displayDetails = (video) => {
+    console.log(video);
+    
+}
+
 // Create Display Categories
 const diplayCategories = (categories) => {
   const categoryContainer = document.getElementById("categories");
   // add data in html
   categories.forEach((item) => {
-    console.log(item);
+    // console.log(item);
     //create a button
     const buttonContainer = document.createElement("div");
     buttonContainer.innerHTML = `
@@ -69,14 +84,14 @@ const diplayCategories = (categories) => {
 };
 
 // Create Display Videos
-const displayVideos = (videos) => {
+const displayVideos = (videos) => {    
   const videoContainer = document.getElementById("videos");
   videoContainer.innerHTML = ""
   if(videos.length == 0){
     videoContainer.classList.remove("grid")
     videoContainer.innerHTML = `
         <div class="min-h-[500] w-full flex flex-col gap-5 justify-center items-center">
-            <img src="assets/icon.png" />
+            <img src="./assets/icon.png" />
             <h2 class="text-center text-xl font-bold">No content Here in this Category</h2>
         </div>
     `
@@ -84,38 +99,37 @@ const displayVideos = (videos) => {
   }else{
     videoContainer.classList.add("grid")
   }
-  videos.forEach((videos) => {
-    console.log(videos);
+  videos.forEach((video) => {
     const card = document.createElement("div");
     card.classList = "card card-compact";
     card.innerHTML = `
     <figure class="h-[200px] relative">
-        <img src=${videos.thumbnail} class="h-full w-full object-cover"
+        <img src=${video.thumbnail} class="h-full w-full object-cover"
         alt="Shoes" />
         ${
-          videos.others.posted_date?.length == 0
+          video.others.posted_date?.length == 0
             ? ""
-            : `<span class="absolute right-2 bottom-2 text-xs bg-black rounded p-1 text-white">${getTimeString(videos.others.posted_date)}</span>`
+            : `<span class="absolute right-2 bottom-2 text-xs bg-black rounded p-1 text-white">${getTimeString(video.others.posted_date)}</span>`
         }
         
   </figure>
   <div class="px-0 py-2 flex gap-2">
         <div>
             <img class="w-10 h-10 rounded-full object-cover" src=${
-              videos.authors[0].profile_picture
+              video.authors[0].profile_picture
             } />
         </div>
          <div>
-            <h2 class="font-bold">${videos.title}</h2>
+            <h2 class="font-bold">${video.title}</h2>
             <div class="flex items-center gap-2">
-                <p class="text-gray-400">${videos.authors[0].profile_name}</p>
+                <p class="text-gray-400">${video.authors[0].profile_name}</p>
                 ${
-                  videos.authors[0].verified == true
+                  video.authors[0].verified == true
                     ? `<img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" />`
                     : ""
                 }
             </div>
-            <p></p>
+            <p> <button onclick="loadDetails('${video.video_id}')" class="btn btn-sm btn-error">Details</button> </p>
         </div>   
 
   </div> 
