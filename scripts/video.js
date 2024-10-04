7ed7feeff7da107bf633f468e81aebc608ff8803
Marina <marinaakter878@gmail.com>
2024-10-04 15:23:53 +0600

    video.js to add dynamic category button functionality

--- scripts/video.js
+++ scripts/video.js
@@ -6,6 +6,15 @@ function getTimeString(time){
     return `${hour} hour ${minute} minute ${remainingSecond} second ago`
 }
 
+const removeActiveClass = () => {
+    const buttons = document.getElementsByClassName('category-btn')
+    console.log(buttons);
+    for(let btn of buttons){
+        btn.classList.remove('active')
+    }
+    
+}
+
 // 1. Fetch, load and show categories on html
 
 // Create loadcategories
@@ -29,7 +38,14 @@ const loadVideos = () => {
 const loadcategoryVideos = (id) => {
     fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
     .then((res) => res.json())
-    .then((data) => displayVideos(data.category))
+    .then((data) => {
+        //sobai k active class remove koro r id er class k active koro
+        removeActiveClass()
+        const activeBtn = document.getElementById(`btn-${id}`)
+        activeBtn.classList.add('active')
+        displayVideos(data.category)
+        
+    })
     .catch((error) => console.log(error));
 }
 
@@ -42,7 +58,7 @@ const diplayCategories = (categories) => {
     //create a button
     const buttonContainer = document.createElement("div");
     buttonContainer.innerHTML = `
-        <button onclick="loadcategoryVideos(${item.category_id})" class="btn">
+        <button id="btn-${item.category_id}" onclick="loadcategoryVideos(${item.category_id})" class="btn category-btn">
             ${item.category}
         </button>
     `
