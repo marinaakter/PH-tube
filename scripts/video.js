990d178acb100c90bbb7e8464aca191f26cd1f0e
Marina <marinaakter878@gmail.com>
2024-10-04 14:14:37 +0600

    video.js to dynamically display video duration

--- scripts/video.js
+++ scripts/video.js
@@ -1,3 +1,11 @@
+function getTimeString(time){
+    const hour = parseInt(time/3600)
+    let remainingSecond = parseInt(time % 3600)
+    const minute = parseInt(remainingSecond / 60)
+    remainingSecond = remainingSecond % 60
+    return `${hour} hour ${minute} minute ${remainingSecond} second ago`
+}
+
 // 1. Fetch, load and show categories on html
 
 // Create loadcategories
@@ -40,30 +48,43 @@ const displayVideos = (videos) => {
   videos.forEach((videos) => {
     console.log(videos);
     const card = document.createElement("div");
-    card.classList = "card card-compact"
+    card.classList = "card card-compact";
     card.innerHTML = `
-    <figure class="h-[200px]">
+    <figure class="h-[200px] relative">
         <img src=${videos.thumbnail} class="h-full w-full object-cover"
         alt="Shoes" />
+        ${
+          videos.others.posted_date?.length == 0
+            ? ""
+            : `<span class="absolute right-2 bottom-2 bg-black rounded p-1 text-white">${getTimeString(videos.others.posted_date)}</span>`
+        }
+        
   </figure>
   <div class="px-0 py-2 flex gap-2">
         <div>
-            <img class="w-10 h-10 rounded-full object-cover" src=${videos.authors[0].profile_picture} />
+            <img class="w-10 h-10 rounded-full object-cover" src=${
+              videos.authors[0].profile_picture
+            } />
         </div>
          <div>
             <h2 class="font-bold">${videos.title}</h2>
             <div class="flex items-center gap-2">
                 <p class="text-gray-400">${videos.authors[0].profile_name}</p>
-                <img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" />
+                ${
+                  videos.authors[0].verified == true
+                    ? `<img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" />`
+                    : ""
+                }
             </div>
             <p></p>
         </div>   
 
   </div> 
         `;
-        videoContainer.appendChild(card)
+    videoContainer.appendChild(card);
   });
 };
 
 loadcategories();
 loadVideos();
+
